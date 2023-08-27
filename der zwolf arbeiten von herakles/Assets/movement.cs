75a4d98565200dc975a42b00b5b4f3be4d4375b2
zakverslinder <143042668+zakverslinder@users.noreply.github.com>
2023-08-27 21:27:32 +0200

    badussy-ussy

--- der zwolf arbeiten von herakles/Assets/movement.cs
+++ der zwolf arbeiten von herakles/Assets/movement.cs
@@ -1,17 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5f;
+    public float jumpSpeed = 8f;
+    private float direction = 0f;
+    private Rigidbody2D player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        if(Input.GetKey(KeyCode.A)){
-            transform.Translate(UnityEngine.Vector3.left * speed * Time.deltaTime);
+        direction = Input.GetAxis("Horizontal");
+
+        if (direction > 0f)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
         }
-        if(Input.GetKey(KeyCode.D)){
-            transform.Translate(UnityEngine.Vector3.right * speed * Time.deltaTime);
+        else if (direction < 0f)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+        }
+        else
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
         }
     }
 }
